207aef8a2b3d8d3db34d9f66c374919cd10b2f40
luis192-12 <1009620222@unajma.edu.pe>
2024-11-24 18:05:15 -0500

    7

--- src/app/auth/data-acces/auth.service.ts
+++ src/app/auth/data-acces/auth.service.ts
@@ -33,7 +33,7 @@ export class AuthService {
   signInWithGoogle() {
     const provider = new GoogleAuthProvider();
 
-    // provider.setCustomParameters({ prompt: 'select_account' });
+    provider.setCustomParameters({ prompt: 'select_account' });
 
     return signInWithPopup(this._auth, provider);
   }
